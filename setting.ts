2bb9e8a32acdfd87cf5470912c54772009b56710
funabiki27 <2415010t@cloud.kobe-u.jp>
2024-08-24 21:44:27 +0900

    Revert "setup ogp"
    This reverts commit a5115455dc0671f429c62664230eae846206abb8.

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://funabiki27.github.io/tech-blog.github.io/";
+export const TARGET_URL = "https://";
